6767b93c1f4ac308d9f0b92287840a6dcdc6a2f2
Chinmaye-5 <chinmayeramesh5@gmail.com>
2026-04-16 16:45:38 +0530

    Update feedRoutes.js

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -1,1 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { getFeed } = require("../services/feedService");
 
+router.get("/feed/:userId", getFeed);
+
+module.exports = router;
