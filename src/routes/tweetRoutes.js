05f9cc43fbbd3195bb9b1be9e75b1c384009203c
Chinmaye-5 <chinmayeramesh5@gmail.com>
2026-04-16 16:45:18 +0530

    Update tweetRoutes.js

--- src/routes/tweetRoutes.js
+++ src/routes/tweetRoutes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { createTweet, likeTweet, deleteTweet, getTweets } = require("../services/tweetService");
 
+router.post("/tweet", createTweet);
+router.post("/like/:id", likeTweet);
+router.delete("/tweet/:id", deleteTweet);
+router.get("/tweets", getTweets);
+
+module.exports = router;
