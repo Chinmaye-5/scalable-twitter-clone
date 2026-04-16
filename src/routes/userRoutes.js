aaeacfd4232505850dd9ef73c8241795120a227c
Chinmaye-5 <chinmayeramesh5@gmail.com>
2026-04-16 16:45:00 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { createUser, followUser } = require("../services/userService");
 
+router.post("/user", createUser);
+router.post("/follow", followUser);
+
+module.exports = router;
