91f361723882b008afef4c8b5130d90a0d628567
Chinmaye-5 <chinmayeramesh5@gmail.com>
2026-04-16 17:34:26 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 
+const config = require("./src/config/config");
+const limiter = require("./src/middleware/rateLimiter");
+const errorHandler = require("./src/middleware/errorHandler");
+
 const userRoutes = require("./src/routes/userRoutes");
 const tweetRoutes = require("./src/routes/tweetRoutes");
 const feedRoutes = require("./src/routes/feedRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
+// Routes
 app.use("/", userRoutes);
 app.use("/", tweetRoutes);
 app.use("/", feedRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+// Error handler
+app.use(errorHandler);
+
+app.listen(config.PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${config.PORT}`);
 });
